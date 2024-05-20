febf29fc99c0ca2de5a48132232edf5f61fdb195
Tom <tom.vandevoorde@gmail.com>
2024-05-20 18:21:03 +0200

    create character

---
+++ src/VirtualDM/VirtualDM.Game/Character.cs
@@ -0,0 +1,8 @@
+﻿namespace VirtualDM.Game
+{
+    internal class Character
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Background { get; set; } = string.Empty;
+    }
+}
---
+++ src/VirtualDM/VirtualDM.Game/Program.cs
@@ -0,0 +1,16 @@
+﻿using VirtualDM.Game;
+
+Console.WriteLine("Welcome to VirtualDM");
+
+var character = new Character();
+
+Console.WriteLine("Please enter your character's name:");
+character.Name = Console.ReadLine()!;
+
+Console.WriteLine("Please enter your character's background:");
+character.Background = Console.ReadLine()!;
+
+Console.WriteLine($"Your character is named {character.Name}");
+Console.WriteLine($"Your character's background is: {character.Background}");
+
+Console.ReadLine();
